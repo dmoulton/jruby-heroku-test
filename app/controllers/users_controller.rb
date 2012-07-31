024a3fa78271647ac7f9185767f2712ef958419d
David Moulton <dave@themoultons.net>
2012-07-31 11:27:56 -0600

    bumping

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,7 @@ class UsersController < ApplicationController
   # GET /users.json
   def index
     @users = User.all
-
+     # adding a comment
     respond_to do |format|
       format.html # index.html.erb
       format.json { render :json => @users }
